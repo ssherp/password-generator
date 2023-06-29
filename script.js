ae35d78b59e08e9ade5160ea9fa7910112b8b6ad
ssherp <ssherpa89@gmail.com>
2023-06-29 15:32:12 -0700

    add vars

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Assignment code here
-var pwlength = "1234567890"
+var randomNum = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var randomUpper=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z","Y","Z"]
+var randomSpec=['!','@','#','$','%','^','&','*','-','=','+']
 
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,33 +13,31 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  //spread oprater//
 }
 
 function generatePassword() {
 
-  var pwlength = prompt("Please provide disired password length:");
+  var pwlength = parseInt(prompt("Please provide disired password length:", "number from 1-128"));
 
-alert("You selected "+ pwlength +" characters.");
-
-if(pwlength >= 8 && pwlength <= 128 ){
-
-confirm (pwlength +" will be your length!!");
-} else {
-
-  alert("ALERT! MUST BE BETWEEN 8-128 CHARACTERS!!!");
+  if (pwlength >= 8 && pwlength <= 128) { 
+    
+  alert("You selected "+ pwlength +" as your length.");  
   
+  }else {
+    alert("ALERT! MUST BE BETWEEN 8-128 CHARACTERS!!!");
+    return;
+  } 
+  
+  var numType = confirm("would you like numbers?");
+
+  var upperType = confirm("would you like upper case letters?");
+
+  var lowerType = confirm("would you like lower case letter?");
+
+  var specType = confirm("would you like special characters?");
 }
-alert("please answer the following prompts with 'yes' or'no' to choose your prefered password settings.");
 
-var numType = prompt("would you like numbers?");
-
-var upperType = prompt("would you like upper case letters?");
-
-var lowerType = prompt("would you like lower case letter?");
-
-var specType = prompt("would you like special characters?");
-} 
 
 
 
