81ddbc28def804d2dd138e86df6ebfe237498ac5
ssherp <ssherpa89@gmail.com>
2023-06-29 15:44:44 -0700

    add if statements

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Assignment code here
 var randomNum = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var randomUpper=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z","Y","Z"]
-var randomSpec=['!','@','#','$','%','^','&','*','-','=','+']
+var randomSpec=['!','@','#','$','%','^','&','*','-','=','+','"',',',"'"];
+var allCharater=[]
 
 // Get references to the #generate element
 
@@ -31,11 +32,31 @@ function generatePassword() {
   
   var numType = confirm("would you like numbers?");
 
+  if (numType){
+
+    allCharater=allCharater.concat(randomNum)
+
+  }
   var upperType = confirm("would you like upper case letters?");
+  
+  if (upperType){
 
+    allCharater=allCharater.concat(randomUpper)
+
+  }
   var lowerType = confirm("would you like lower case letter?");
+  if (lowerType){
 
+    allCharater=allCharater.concat(randomLower)
+
+  }
   var specType = confirm("would you like special characters?");
+
+  if (specType){
+
+    allCharater=allCharater.concat(randomSpec)
+
+  }
 }
 
 
